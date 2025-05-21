d4c99dd8849962a5d4ab0b32c52ebf88bfe81233
Nurzakiyah23 <nurzakiyahtamsirzakiyah@gmail.com>
2025-05-21 15:05:45 +0800

    laporan 2

--- Nur_Zakiyah_Tamsir/routes/web.php
+++ Nur_Zakiyah_Tamsir/routes/web.php
@@ -8,19 +8,15 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
-
 Route::get('/skills', function () {
     return view('skills');
- });
-
- Route::get('/contact', function () {
-    return view('contact');
- });
-
- Route::get('/home', function () {
+});
+Route::get('/contact', function () {
+   return view('contact');
+});
+Route::get('/home', function () {
    return view('home');
 });
-
 Route::get('/project', function () {
     return view('project');
 });
