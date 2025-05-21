0b2430fec399146426ef8e29e26da3062a12d7a7
Nurzakiyah23 <nurzakiyahtamsirzakiyah@gmail.com>
2025-05-21 15:03:59 +0800

    dari kau

---
+++ Nur_Zakiyah_Tamsir/resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <a href="/about">about</a>
+    <a href="">contac</a>
+     <h1>Halaman Home</h1>
+    <p>Selamat datang di website portofolio saya.</p>
+</body>
+</html>
---
+++ Nur_Zakiyah_Tamsir/resources/views/skills.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skills</title>
+</head>
+<body>
+     <h1>Kemampuan Saya</h1>
+    <ul>
+        <li>HTML, CSS, JavaScript</li>
+        <li>PHP, Laravel</li>
+        <li>MySQL, Git</li>
+    </ul>
+</body>
+</html>
