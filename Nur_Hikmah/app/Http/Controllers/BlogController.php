648cbf9bbab8aea74e4a219c9ff578e3dba7ff56
nhcimma <nhnurhikmah675@gmail.com>
2025-06-04 22:05:57 +0800

    Revisi Laporan 3

--- Nur_Hikmah/app/Http/Controllers/BlogController.php
+++ Nur_Hikmah/app/Http/Controllers/BlogController.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class BlogController
+class BlogController extends Controller
 {
-    //
+    public function index()
+    {
+        $blog = [
+            'Laporan 1' => 'Pengenalan Laravel dan Dasar Pemrograman PHP',
+            'Laporan 2' => 'Web Portofolio Laravel Part I',
+            'Laporan 3' => 'Web Portofolio Laravel Part II',
+        ];
+        return view('blog', compact('blog'));
+    }
+
+    public function show($id) {
+        if($id == 'Laporan 1') {
+            return redirect('biodata');
+        } else if($id == 'Laporan 2') {
+            return redirect('about');
+        } else if($id == 'Laporan 3') {
+            return redirect('home');
+        }
+    }
 }
--- Nur_Hikmah/app/Http/Controllers/SkillController.php
+++ Nur_Hikmah/app/Http/Controllers/SkillController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class SkillController
+class SkillController extends Controller
 {
-    //
+    public function index()
+    {
+        $skill = [
+            'Semester 1' => [
+                'Algoritma dan Pemrograman',
+                'Pengenalan Teknologi Informasi dan Ilmu Komputer',
+            ],
+            'Semester 2' => [
+                'Pemrograman Terstruktur',
+                'Sistem Operasi Komputer',
+                'Fisika Terapan',
+                'Elektronika Digital',
+            ],
+            'Semester 3' => [
+                'Struktur Data',
+                'Pemrograman Web 1',
+                'Pemrograman Berorientasi Objek',
+                'Sistem Tertanam',
+            ],
+            'Semester 4' => [
+                'Pemrograman Perangkat Bergerak',
+                'Pemrograman Web 2',
+                'Statistika dan Probabilitas',
+                'Jaringan Komputer',
+            ],
+        ];
+        return view('skill', compact('skill'));
+    }
 }
